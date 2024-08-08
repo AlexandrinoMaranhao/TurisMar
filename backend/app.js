883ebbb13984f6dcae60dc5104873af6f15a542e
Marcio "Alexandrino" Sá <78682655+AlexandrinoMaranhao@users.noreply.github.com>
2024-08-08 07:33:03 -0300

    fixing CSP

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,20 @@ const conn =  require("./db/conn");
 const PORT = 3000;
 const cors =  require("cors");
 const path = require("path");
+const helmet =  require("helmet");
 
 console.log('Current directory:', __dirname); 
 //Iniciando a API
 app.use(cors())
 app.use(express.json())
-
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'none'"],
+      imgSrc: ["'self'", "https://turismar-app.onrender.com"],
+      scriptSrc: ["'self'"],
+      styleSrc: ["'self'"],
+    }
+}));
 
 //Conectando o DB
 conn();
